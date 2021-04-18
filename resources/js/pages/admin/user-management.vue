187fd20c8f253dd09e8d20dbd4e8803ca3c82df5
Terry Mun <hello@terrymun.com>
2021-04-18 10:28:22 +0200

    Improved sorting on users table

--- resources/js/pages/admin/user-management.vue
+++ resources/js/pages/admin/user-management.vue
@@ -16,56 +16,84 @@
 		</div>
 
 		<!-- Users table -->
-		<table
-			v-else
-			class="table table-hover">
-			<thead>
-				<tr>
-					<th scope="col" rowspan="2" v-on:click="sortByField('name')" data-sortable>
-						<fa v-bind:class="getSortIconCssClassObjectByField('name')" v-bind:icon="getSortIconByField('name')" fixed-width />
-						Name
-					</th>
-					<th scope="col" rowspan="2" v-on:click="sortByField('email')" data-sortable>
-						<fa v-bind:class="getSortIconCssClassObjectByField('email')" v-bind:icon="getSortIconByField('email')" fixed-width />
-						Email
-					</th>
-					<th v-bind:colspan="rolesColSpan">Roles</th>
-				</tr>
-				<tr>
-					<th
-						scope="col"
-						v-for="(role, i) in roles"
-						v-bind:key="i">
-						{{ roleName[role] }}
-					</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr
-					v-for="(user, i) in users"
-					v-bind:key="i">
-					<td>{{ user.name }}</td>
-					<td>{{ user.email }}</td>
-					<td
-						class="text-center"
-						v-for="(role, j) in roles"
-						v-bind:key="j">
-						<div class="custom-control custom-switch">
-							<input
-								type="checkbox"
-								class="custom-control-input"
-								v-model="user[role]"
-								v-on:input="onToggleInput(user, role, $event)"
-								v-bind:id="getToggleId(role, i)" />
-							<label
-								class="custom-control-label"
-								v-bind:for="getToggleId(role, i)"
-								v-bind:title="getToggleTitle(user, role)"></label>
+		<template v-else>
+			<form class="mb-3 px-4 py-3 bg-light">
+				<fieldset>
+					<legend>Filter</legend>
+					<div class="row">
+						<label for="filter--project" class="col-sm-2 col-form-label col-form-label-sm">Project</label>
+						<div class="col-sm-10">
+							<select
+								id="filter--project"
+								class="custom-select custom-select-sm"
+								v-model="project">
+								<option selected value="">Select a project</option>
+								<option value="norfab">NorFab</option>
+								<option value="profaba">ProFaba</option>
+							</select>
 						</div>
-					</td>
-				</tr>
-			</tbody>
-		</table>
+					</div>
+				</fieldset>
+			</form>
+
+			<div v-if="!hasUsers" class="alert alert-secondary" role="alert">
+				No users match the filtering criteria.
+			</div>
+
+			<table v-else class="table table-hover">
+				<thead>
+					<tr>
+						<th class="text-nowrap" scope="col" rowspan="2" v-on:click="sortByField('name')" data-sortable>
+							<fa v-bind:class="getSortIconCssClassObjectByField('name')" v-bind:icon="getSortIconByField('name')" fixed-width />
+							Name
+						</th>
+						<th class="text-nowrap" scope="col" rowspan="2" v-on:click="sortByField('email')" data-sortable>
+							<fa v-bind:class="getSortIconCssClassObjectByField('email')" v-bind:icon="getSortIconByField('email')" fixed-width />
+							Email
+						</th>
+						<th class="text-nowrap" scope="col" rowspan="2" v-on:click="sortByField('email_verified_at')" data-sortable>
+							<fa v-bind:class="getSortIconCssClassObjectByField('email_verified_at')" v-bind:icon="getSortIconByField('email_verified_at')" fixed-width />
+							Verified on
+						</th>
+						<th v-bind:colspan="rolesColSpan">Roles</th>
+					</tr>
+					<tr>
+						<th
+							scope="col"
+							v-for="(role, i) in roles"
+							v-bind:key="i">
+							{{ roleName[role] }}
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr
+						v-for="(user, i) in usersFiltered"
+						v-bind:key="i">
+						<td>{{ user.name }}</td>
+						<td class="text-break" v-html="getBreakableEmail(user.email)"></td>
+						<td>{{ getFormattedTime(user.email_verified_at) }}</td>
+						<td
+							class="text-center"
+							v-for="(role, j) in roles"
+							v-bind:key="j">
+							<div class="custom-control custom-switch">
+								<input
+									type="checkbox"
+									class="custom-control-input"
+									v-model="user[role]"
+									v-on:input="onToggleInput(user, role, $event)"
+									v-bind:id="getToggleId(role, i)" />
+								<label
+									class="custom-control-label"
+									v-bind:for="getToggleId(role, i)"
+									v-bind:title="getToggleTitle(user, role)"></label>
+							</div>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</template>
 	</div>
 </template>
 
@@ -78,6 +106,7 @@ export default {
 
 	data: function() {
 		return {
+			project: '',
 			users: [],
 			roles: [
 				'is_norfab_user',
@@ -99,10 +128,25 @@ export default {
 	},
 
 	computed: {
-		rolesColSpan() {
+		usersFiltered: function() {
+			return this.users
+				.filter(user => {
+					switch (this.project) {
+						case 'norfab':
+							return user.is_norfab_user;
+						case 'profaba':
+							return user.is_profaba_user;
+						default:
+							return true;
+					}
+				});
+		},
+		hasUsers: function() {
+			return !!this.usersFiltered.length;
+		},
+		rolesColSpan: function() {
 			return this.roles.length;
 		},
-
 	},
 
 	methods: {
@@ -158,6 +202,21 @@ export default {
 			};
 		},
 
+		getBreakableEmail(email) {
+			return email.replace('@', '<wbr>@');
+		},
+
+		getFormattedTime(dateString) {
+			const dateTimeFormat = new Intl.DateTimeFormat('en-GB', {
+				year: 'numeric',
+				month: 'short',
+				day: 'numeric',
+				hour: '2-digit',
+				minute: '2-digit',
+			});
+			return dateTimeFormat.format(new Date(dateString));
+		},
+
 		sortByField(field) {
 			switch (field) {
 				case 'name':
@@ -192,6 +251,23 @@ export default {
 						}
 
 						return a.email.localeCompare(b.email) * (this.sortState === 2 ? -1 : 1);
+					});
+					break;
+
+				case 'email_verified_at':
+					if (this.sortField !== field) {
+						this.sortState = 0;
+					}
+
+					this.sortField = field;
+					this.sortState = (this.sortState + 1) % 3;
+
+					this.users.sort((a, b) => {
+						if (this.sortState === 0) {
+							return a.originalIndex - b.originalIndex;
+						}
+
+						return (new Date(a.email_verified_at) - new Date(b.email_verified_at)) * (this.sortState === 2 ? -1 : 1);
 					});
 					break;
 			}
